f5e7e57d7dfea9a0037b8faab0e63ea4ff06b9df
EpocDotFr <contact@epoc.fr>
2018-01-31 08:52:30 +0100

    Put hooks in their own file

---
+++ hooks.py
@@ -0,0 +1,52 @@
+from flask import render_template, request, g, make_response
+from werkzeug.exceptions import HTTPException
+from webtodotxt import app, babel, auth
+
+
+@app.before_request
+def set_locale():
+    if not hasattr(g, 'CURRENT_LOCALE'):
+        if app.config['FORCE_LANGUAGE']:
+            g.CURRENT_LOCALE = app.config['FORCE_LANGUAGE']
+        else:
+            g.CURRENT_LOCALE = request.accept_languages.best_match(app.config['LANGUAGES'].keys(), default=app.config['DEFAULT_LANGUAGE'])
+
+
+@auth.get_password
+def get_password(username):
+    if username in app.config['USERS']:
+        return app.config['USERS'].get(username)
+
+    return None
+
+
+@auth.error_handler
+def auth_error():
+    return http_error_handler(403, without_code=True)
+
+
+@babel.localeselector
+def get_app_locale():
+    if not hasattr(g, 'CURRENT_LOCALE'):
+        return app.config['DEFAULT_LANGUAGE']
+    else:
+        return g.CURRENT_LOCALE
+
+
+@app.errorhandler(401)
+@app.errorhandler(403)
+@app.errorhandler(404)
+@app.errorhandler(500)
+@app.errorhandler(503)
+def http_error_handler(error, without_code=False):
+    if isinstance(error, HTTPException):
+        error = error.code
+    elif not isinstance(error, int):
+        error = 500
+
+    body = render_template('errors/{}.html'.format(error))
+
+    if not without_code:
+        return make_response(body, error)
+    else:
+        return make_response(body)
--- webtodotxt.py
+++ webtodotxt.py
@@ -1,8 +1,7 @@
-from flask import Flask, render_template, request, g, make_response
 from logging.handlers import RotatingFileHandler
-from werkzeug.exceptions import HTTPException
 from flask_httpauth import HTTPBasicAuth
 from flask_babel import Babel
+from flask import Flask
 import logging
 
 
@@ -41,60 +40,4 @@ from helpers import *
 
 
 import routes
-
-
-# -----------------------------------------------------------
-# HTTP errors handler
-
-
-@app.errorhandler(401)
-@app.errorhandler(403)
-@app.errorhandler(404)
-@app.errorhandler(500)
-@app.errorhandler(503)
-def http_error_handler(error, without_code=False):
-    if isinstance(error, HTTPException):
-        error = error.code
-    elif not isinstance(error, int):
-        error = 500
-
-    body = render_template('errors/{}.html'.format(error))
-
-    if not without_code:
-        return make_response(body, error)
-    else:
-        return make_response(body)
-
-
-# -----------------------------------------------------------
-# Hooks
-
-
-@app.before_request
-def set_locale():
-    if not hasattr(g, 'CURRENT_LOCALE'):
-        if app.config['FORCE_LANGUAGE']:
-            g.CURRENT_LOCALE = app.config['FORCE_LANGUAGE']
-        else:
-            g.CURRENT_LOCALE = request.accept_languages.best_match(app.config['LANGUAGES'].keys(), default=app.config['DEFAULT_LANGUAGE'])
-
-
-@auth.get_password
-def get_password(username):
-    if username in app.config['USERS']:
-        return app.config['USERS'].get(username)
-
-    return None
-
-
-@auth.error_handler
-def auth_error():
-    return http_error_handler(403, without_code=True)
-
-
-@babel.localeselector
-def get_app_locale():
-    if not hasattr(g, 'CURRENT_LOCALE'):
-        return app.config['DEFAULT_LANGUAGE']
-    else:
-        return g.CURRENT_LOCALE
+import hooks
